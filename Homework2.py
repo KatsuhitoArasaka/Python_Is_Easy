7eac838a82222c688626beb54900989cb15960fc
NGS76 <46249418+NGS76@users.noreply.github.com>
2019-01-12 20:29:10 +0300

    Create Homework2.py

---
+++ Homework2.py
@@ -0,0 +1,19 @@
+def title():
+    title_of_song = "Killer Queen"
+    print(title_of_song)
+    return title_of_song
+
+def band():
+    name_of_band = "Queen"
+    print(name_of_band)
+    return name_of_band
+
+def author():
+    name_of_author = "Freddie Mercury"
+    print(name_of_author)
+    return name_of_author
+
+bool(title_of_song)
+bool(name_of_band)
+bool(name_of_author)
+
