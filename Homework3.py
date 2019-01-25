c9e82cfb21488329edd0c26d89f1d26a1cad7998
NGS76 <46249418+NGS76@users.noreply.github.com>
2019-01-25 18:14:47 +0200

    Create Homework3.py

---
+++ Homework3.py
@@ -0,0 +1,23 @@
+def title():
+    output = "Killer Queen"
+    return output
+a = title()
+print(a)
+a1 = bool(a)
+print(a1)
+
+def band():
+    output = "Queen"
+    return output
+b = band()
+print(b)
+b1 = bool(b)
+print(b1)
+          
+def author():
+    output = "Freddie Mercury"
+    return output
+c = author()
+print(c)
+c1 = bool(c)
+print(c1)
